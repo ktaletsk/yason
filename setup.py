34e8cf7fe372b24b2dd2cc03ba25b137b83945dd
Konstantin Taletskiy <konstantin@taletskiy.com>
2019-05-28 14:26:57 +0000

    test: Changes to be compatible with Python 3.4 and 3.5

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', 'boto3>=1.9.0', 'pandas>=0.24.0', 'kubernetes>=9.0.0']
+requirements = ['Click>=6.0', 'boto3>=1.9.0', 'pandas>=0.20.0', 'kubernetes>=9.0.0']
 
 setup_requirements = ['pytest-runner', ]
 
-test_requirements = ['pytest', 'boto3>=1.9.0', 'pandas>=0.24.0', 'kubernetes>=9.0.0']
+test_requirements = ['pytest', 'boto3>=1.9.0', 'pandas>=0.20.0', 'kubernetes>=9.0.0']
 
 setup(
     author="Konstantin Taletskiy",
